bcf2c1e98baa59d8e0ac9a4ac70c0e1c0ca02806
The-LukeZ <luke.hent3005@gmail.com>
2024-12-22 04:56:40 +0100

    feat: update impressum with new contact information and responsible party details

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -19,14 +19,22 @@
     <p>Angaben gemäß § 5 TMG</p>
 
     <ul class="list-none">
-      <li class="mb-0 mt-0">Max Mustermann</li>
-      <li class="mb-0 mt-0">Musterstraße 1</li>
-      <li class="mb-0 mt-0">12345 Musterstadt</li>
+      <li class="mb-0 mt-0">Jan Mehlhorn</li>
+      <li class="mb-0 mt-0">Poststraße 10</li>
+      <li class="mb-0 mt-0">09353 Oberlungwitz</li>
       <li class="mb-0 mt-0">Deutschland</li>
     </ul>
     <p>
       Email: <a href="mailto:burningdezibelz@gmail.com" target="_blank" class="dy-link-primary">burningdezibelz@gmail.com</a>
     </p>
+    <hr />
+    <p><b>Verantwortlich für den Inhalt der Seiten:</b></p>
+    <ul class="list-none">
+      <li class="mb-0 mt-0">Luca Hentschel</li>
+      <li class="mb-0 mt-0">Schneeberger Straße 31</li>
+      <li class="mb-0 mt-0">08112 Wilkau-Haßlau</li>
+      <li class="mb-0 mt-0">Deutschland</li>
+    </ul>
   </article>
 </div>
 
