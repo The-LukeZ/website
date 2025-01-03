6c36b544443c8d22d26127aa6a69e07a888748e9
The-LukeZ <luke.hent3005@gmail.com>
2025-01-03 20:21:06 +0100

    fix: disable prerendering for layout and about pages

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,6 @@
 import ky from "ky";
 import { PUBLIC_BASE_URL } from "$env/static/public";
 
-export const prerender = false;
-
 export async function load() {
   const _res = await ky.get(PUBLIC_BASE_URL + "/api/date").json();
   return {
--- src/routes/about/+page.server.js
+++ src/routes/about/+page.server.js
@@ -1,7 +1,5 @@
 import bandMembers from "$lib/bandMembers.json";
 
-export const prerender = true;
-
 export function load() {
   return {
     members: bandMembers,
