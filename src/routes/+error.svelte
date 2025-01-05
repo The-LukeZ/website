09b8a475f4d5d3d41dae9cdda571d6a0c295b0d8
The-LukeZ <luke.hent3005@gmail.com>
2025-01-05 15:02:19 +0100

    feat: add custom error page with status images

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { page } from "$app/state";
+</script>
+
+<div class="my-auto flex h-full flex-col items-center justify-center p-2 md:p-10">
+  <img
+    src="https://http.cat/{page.status}"
+    alt="{page.status} {page.error.message}"
+    class="size-fit rounded-xl shadow-lg shadow-primary-content"
+  />
+</div>
